e987abe1cd9edf22491b11175d5e773678611024
Gregory Ditzler <gregory.ditzler@gmail.com>
2021-06-12 14:55:03 -0400

    added template to leveliw and import from __init__

--- models/__init__.py
+++ models/__init__.py
@@ -5,3 +5,9 @@ from .compose import ComposeV1
 from .compose import ComposeV2
 from .compose import FastCompose
 
+from .mclassification import MClassification
+
+from .leveliw import LevelIW
+
+from .scargc import Scargc
+
--- models/leveliw.py
+++ models/leveliw.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python 
+
+
+
+class LevelIW(): 
+    def __init__(self, 
+                 classifier, 
+                 method): 
+        """
+        """
+        self.classifier = classifier 
+    
+    def run(self, Xt, Yt, Ut): 
+        """
+        """
+        self.classifier
